acd4f335674ed955e16ef1c7bc4c6f9c4f82a575
Shoka Shimizu <aaxa2108@chiba-u.jp>
2017-08-20 22:14:09 +0900

    サンプル作り直してる

--- sample_app/app.py
+++ sample_app/app.py
@@ -2,25 +2,40 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, jsonify, request
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, create_engine
+
+
+from sqlalchemy.ext.declarative import declarative_base
 
 app = Flask(__name__)
+
+class User(object):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __init__(self, name):
+        self.name = name
 
 @app.route('/scores', methods=['GET', 'POST'])
 def scores_view():
     result = ''
 
     if request.method == 'POST':
-        result = post_scores()
+        result = post_score()
     if request.method == 'GET':
-        result = get_scores()
+        result = get_score(request)
 
     return result
 
-def post_scores():
+def post_score():
     return 'POST'
 
-def get_scores():
-    return 'GET'
+def get_score(request):
+    limit = request.args.get('limit')
+    if limit is None:
+        limit = '10'
+    return limit
 
 if __name__ == '__main__':
     app.run(debug=True)
--- sample_app/responce_specification.md
+++ sample_app/responce_specification.md
@@ -3,19 +3,25 @@
 ## scores
 
 ### method:GET
+スコアを取得する
 
 #### optional parameter
 
 ##### sort
 
-
 ##### limit
+デフォルト10
 
 ### method:post
 
 
 # DB
 
+```
+app(id,token, app_name)
+
 user(id,name)
 
-score(id, user_id, score, message)
+score(id, app_id, user_id, score, message)
+
+```
